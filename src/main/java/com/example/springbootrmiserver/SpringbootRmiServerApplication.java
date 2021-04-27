85cc90a303f2d14e26b7619fbfb80399dd543a45
zxz <1249364874@qq.com>
2021-04-27 15:43:50 +0800

    qqq

---
+++ src/main/java/com/example/springbootrmiserver/SpringbootRmiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootrmiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRmiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRmiServerApplication.class, args);
+    }
+
+}
